32574bafe41a6edf118f14052b22afcd22ed02da
danielle <dmlicea@gmail.com>
2019-03-23 17:41:46 -0500

    saturday

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendsArray = [
 
     {
-		name: 'name',
-		photo: 'photo',
+		name: 'Anala',
+		photo: 'https://placebear.com/200/276',
         scores: 
         
         [
@@ -17,8 +17,218 @@ var friendsArray = [
 			'5',
 			'3'
 		]
-    },
+		},
+		
+	{
+		name: 'Lily',
+		photo: 'https://placebear.com/200/279',
+		scores: 
+					
+		[
+			'3',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'2'
+		]
+		},
+
+  {
+		name: 'Claire',
+		photo: 'https://placebear.com/200/280',
+        scores: 
+        
+        [
+			'1',
+			'3',
+			'2',
+			'2',
+			'4',
+			'2',
+			'3',
+			'2',
+			'5',
+			'2'
+		]
+		},
+		
+	{
+		name: 'Isabella',
+		photo: 'https://placebear.com/200/281',
+		scores: 
+					
+		[
+			'3',
+			'2',
+			'1',
+			'1',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'1'
+		]
+		},
+
+	{
+		name: 'Tobias',
+		photo: 'https://placebear.com/200/282',
+		scores: 
+			
+		[
+			'1',
+			'4',
+			'4',
+			'5',
+			'3',
+			'4',
+			'4',
+			'4',
+			'5',
+			'1'
+		]
+		},
+			
+	{
+		name: 'Dante',
+		photo: 'https://placebear.com/200/283',
+		scores: 
+						
+		[
+			'3',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1'
+		]
+		},
+
+	{
+		name: 'Calliope',
+		photo: 'https://placebear.com/200/284',
+		scores: 
+			
+		[
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1',
+			'4',
+			'4',
+			'5',
+			'4'
+		]
+		},
+			
+	{
+		name: 'Diego',
+		photo: 'https://placebear.com/200/285',
+		scores: 
+						
+		[
+			'3',
+			'3',
+			'3',
+			'3',
+			'2',
+			'2',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+		},
+	
+	{
+		name: 'Mitsuko',
+		photo: 'http://placekitten.com/200/289',
+		scores: 
+			
+		[
+			'1',
+			'2',
+			'2',
+			'2',
+			'3',
+			'2',
+			'2',
+			'2',
+			'2',
+			'3'
+		]
+		},
+			
+	{
+		name: 'Dawn',
+		photo: 'http://placekitten.com/200/288',
+		scores: 
+						
+		[
+			'1',
+			'2',
+			'3',
+			'2',
+			'2',
+			'1',
+			'2',
+			'1',
+			'1',
+			'1'
+		]
+		},
+	
+	{
+		name: 'Hiro',
+		photo: 'http://placekitten.com/200/285',
+		scores: 
+				
+		[
+			'1',
+			'3',
+			'2',
+			'1',
+			'2',
+			'1',
+			'1',
+			'1',
+			'1',
+			'3'
+		]
+		},
+				
+	{
+		name: 'Iris',
+		photo: 'http://placekitten.com/200/287',
+		scores: 
+							
+		[
+			'5',
+			'5',
+			'5',
+			'3',
+			'1',
+			'5',
+			'1',
+			'5',
+			'1',
+			'1'
+		]
+		},
     
 
 ];
+
 module.exports = friendsArray;
